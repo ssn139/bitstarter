27e812471a422af05d9b25a6f5fd0bff922b3ec5
Mark Chapman <ssn139@gmail.com>
2013-07-07 07:17:38 +0000

    web.js updated to not be stupid

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 var buf = new Buffer(8);
-var content = fs.readFileSync(index.html, 'utf-8');
+var content = fs.readFileSync('index.html', 'utf-8');
 buf.write(content, 'utf-8');
 
 app.get('/', function(request, response) {
