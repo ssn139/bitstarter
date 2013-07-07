a6069a90d8d9d89a4eeb8dfceaa100f2f0b209d5
Mark Chapman <ssn139@gmail.com>
2013-07-07 07:13:48 +0000

    web.js version 4

--- web.js
+++ web.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
-var content = fs.readFileSync(index.html);
-var buf = new Buffer(content, 'utf-8');
+var buf = new Buffer(8);
+var content = fs.readFileSync(index.html, 'utf-8');
+buf.write(content, 'utf-8');
 
 app.get('/', function(request, response) {
   response.send(buf.toString('utf-8'));
