0fd40f7830b20ff53f7bcda2a8f42f1f65fb3405
Mark Chapman <ssn139@gmail.com>
2013-07-07 06:58:33 +0000

    web.js v3

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
-var buffer = new Buffer();
-var content = buffer.toString(fs.readFile(index.html));
+var content = fs.readFileSync(index.html);
+var buf = new Buffer(content, 'utf-8');
 
 app.get('/', function(request, response) {
-  response.send(content);
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
