be306660379f816777605febceb84bc0f00da400
Mark Chapman <ssn139@gmail.com>
2013-07-07 06:42:06 +0000

    Updated to load from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
+var buffer = new Buffer();
+var content = buffer.toString(fs.readFile(index.html));
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
