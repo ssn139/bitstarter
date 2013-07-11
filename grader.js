dba155754d03b3a380f792fd7250390b29e4b598
Mark Chapman <ssn139@gmail.com>
2013-07-11 05:34:26 +0000

    Added URL option

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var sys = require('util'), rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -66,8 +67,16 @@ if(require.main == module) {
 	program
 		.option('-c, --checks <check_file>', 'Paths to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 		.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+                .option('-u, --url <url_address>', 'Path to url website')
 		.parse(process.argv);
-	var checkJson = checkHtmlFile(program.file, program.checks);
+         var htmlfile = program.file;
+         if(program.url) {
+	    rest.get(program.url).on('complete', function(result){
+		       fs.writeFileSync("url.html", result);
+		});
+	         htmlFile = "url.html";
+    }
+	var checkJson = checkHtmlFile(htmlFile, program.checks);
 	var outJson = JSON.stringify(checkJson, null, 4);
 	console.log(outJson);
 } else {
